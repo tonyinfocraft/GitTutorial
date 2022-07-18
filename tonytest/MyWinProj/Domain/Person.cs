a178e7d2f5b8f187bc0365794b086ebaa8be5dfc
Tony <tony@infocraft.com.tw>
2022-07-18 11:53:48 +0800

    add Person.cs

---
+++ tonytest/MyWinProj/Domain/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWinProj.Domain
+{
+    public class Person
+    {
+        private string id;
+        private string name;
+
+        public Person(string id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public string Id
+        {
+            get { return this.id; }
+        }
+
+        public string Name
+        {
+            get { return this.id; }
+            set { this.name = value; }
+        }
+    }
+}
